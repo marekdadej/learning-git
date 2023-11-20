071ffa7beebe36156f42cb0d4168bc92b6111c40
Marek Dadej <marek.dadej03@gmail.com>
2023-11-20 17:48:03 +0100

    add function

---
+++ lista_zakupow.py
@@ -0,0 +1,15 @@
+print("Lista zakupów")
+
+list = {
+    "Piekrnia": ["Chleb", "Bułki", "Pączek"],
+    "Warzywniak": ["Marchew", "Seler", "Rukola"]
+}
+
+total_products = 0
+
+for sklep, produkty in list.items():
+    produkty_wielka_litera = [produkt.capitalize() for produkt in produkty]
+    total_products += len(produkty)
+    print(f"Idę do {sklep.capitalize()}, kupuję tu następujące rzeczy: {produkty_wielka_litera}.")
+
+print(f"W sumie kupuję {total_products} produktów.")
