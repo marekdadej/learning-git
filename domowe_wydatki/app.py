0328d5f1e86861e14564e349339fea2b67392086
Marek Dadej <marek.dadej03@gmail.com>
2024-01-12 12:59:40 +0100

    add REST API

--- domowe_wydatki/app.py
+++ domowe_wydatki/app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, jsonify, request, abort, render_template, redirect, url_for
 from forms import ExpenseForm
 from models import Expenses
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'TwojTajnyKlucz'
+app.config['SECRET_KEY'] = 'l1hunti'
 
 expenses = Expenses()
 
@@ -28,5 +28,50 @@ def edit(id):
 
     return render_template('edit.html', form=form, id=id)
 
+# Endpointy API
+@app.route('/api/expenses', methods=['GET'])
+def api_get_expenses():
+    return jsonify(expenses.get_all())
+
+@app.route('/api/expenses', methods=['POST'])
+def api_create_expense():
+    form = ExpenseForm(request.form)
+    if form.validate():
+        expenses.add({'title': form.title.data, 'amount': str(form.amount.data)})
+        return jsonify({"message": "Expense added"}), 201
+    else:
+        return jsonify(form.errors), 400
+
+@app.route('/api/expenses/<int:id>', methods=['GET'])
+def api_get_expense(id):
+    try:
+        expense = expenses.get(id)
+        return jsonify(expense)
+    except IndexError:
+        abort(404)
+
+@app.route('/api/expenses/<int:id>', methods=['PUT'])
+def api_update_expense(id):
+    try:
+        expense = expenses.get(id)
+        form = ExpenseForm(request.form)
+        if form.validate():
+            expenses.update(id, {'title': form.title.data, 'amount': str(form.amount.data)})
+            return jsonify({"message": "Expense updated"}), 200
+        else:
+            return jsonify(form.errors), 400
+    except IndexError:
+        abort(404)
+
+@app.route('/api/expenses/<int:id>', methods=['DELETE'])
+def api_delete_expense(id):
+    try:
+        expenses.delete(id)
+        return jsonify({"message": "Expense deleted"}), 200
+    except IndexError:
+        abort(404)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
